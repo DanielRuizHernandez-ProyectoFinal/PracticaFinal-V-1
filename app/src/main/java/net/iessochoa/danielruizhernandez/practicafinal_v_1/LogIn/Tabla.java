114e4cfd4d0da4d2d900d4109e9fcb0d5a921dcd
Danirzhz <danirzhz@gmail.com>
2022-06-09 00:30:18 +0200

    Commit

--- app/src/main/java/net/iessochoa/danielruizhernandez/practicafinal_v_1/LogIn/Tabla.java
+++ app/src/main/java/net/iessochoa/danielruizhernandez/practicafinal_v_1/LogIn/Tabla.java
@@ -1,6 +1,38 @@
 package net.iessochoa.danielruizhernandez.practicafinal_v_1.LogIn;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import net.iessochoa.danielruizhernandez.practicafinal_v_1.R;
 
 public class Tabla extends Activity {
+
+
+
+    private Button btnSalir;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabla);
+
+
+        btnSalir = findViewById(R.id.btnSalir);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSalir.setOnClickListener(view -> {
+            mAuth.signOut();
+            startActivity(new Intent(this, LoginActivity.class));
+
+        });
+
+    }//End onCreate
+
 }
--- app/src/main/java/net/iessochoa/danielruizhernandez/practicafinal_v_1/MainActivity.java
+++ app/src/main/java/net/iessochoa/danielruizhernandez/practicafinal_v_1/MainActivity.java
@@ -2,16 +2,36 @@ package net.iessochoa.danielruizhernandez.practicafinal_v_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import net.iessochoa.danielruizhernandez.practicafinal_v_1.LogIn.LoginActivity;
+import net.iessochoa.danielruizhernandez.practicafinal_v_1.LogIn.Tabla;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }else{
+            startActivity(new Intent(MainActivity.this, Tabla.class));
+        }
     }
 
 
