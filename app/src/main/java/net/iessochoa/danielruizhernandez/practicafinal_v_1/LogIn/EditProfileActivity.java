4bc339697f654248c7b9f72ffa8d8e0c8b2a781a
Danirzhz <danirzhz@gmail.com>
2022-06-16 01:53:28 +0200

    Commit

--- app/src/main/java/net/iessochoa/danielruizhernandez/practicafinal_v_1/LogIn/EditProfileActivity.java
+++ app/src/main/java/net/iessochoa/danielruizhernandez/practicafinal_v_1/LogIn/EditProfileActivity.java
@@ -3,12 +3,17 @@ package net.iessochoa.danielruizhernandez.practicafinal_v_1.LogIn;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -25,12 +30,15 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import net.iessochoa.danielruizhernandez.practicafinal_v_1.R;
 
+import java.util.HashMap;
+
 public class EditProfileActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore fStore;
     private DatabaseReference mDatabase;
-    EditText etNombre, etCorreo, etTelefono, etContrasenya;
+    private Button btGuardar;
+    private EditText etNombre, etCorreo, etTelefono, etContrasenya;
     String userId;
 
     @Override
@@ -42,7 +50,7 @@ public class EditProfileActivity extends AppCompatActivity {
         etCorreo = findViewById(R.id.etCorreo);
         etTelefono = findViewById(R.id.etTelefono);
         etContrasenya = findViewById(R.id.etContrasenya);
-
+        btGuardar=findViewById(R.id.btGuardar);
 
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
@@ -61,6 +69,42 @@ public class EditProfileActivity extends AppCompatActivity {
                 etContrasenya.setText(documentSnapshot.getString("Contraseña"));
             }
         });
+
+        btGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nombre= etNombre.getText().toString();
+                String correo= etCorreo.getText().toString();
+                String telefono= etTelefono.getText().toString();
+                String contrasenya= etContrasenya.getText().toString();
+
+                updateData(nombre,correo,telefono,contrasenya);
+
+            }
+        });
+    }
+    private void updateData(String nombre, String correo, String telefono, String contrasenya) {
+
+       /* HashMap user= new HashMap();
+        user.put("Nombre",nombre);
+        user.put("Correo",correo);
+        user.put("Teléfono",telefono);
+        user.put("Contraseña",contrasenya);
+
+        */
+
+
+      //  mDatabase=FirebaseDatabase.getInstance().getReference("users");
+      //  mDatabase.child(userId).child("Nombre").setValue(nombre);
+
+        DatabaseReference reference ;
+        //reference=FirebaseDatabase.getInstance().getReference("users");
+       // reference.child("users").child(userId).child("Nombre").setValue(nombre);
+
+
+
+
     }
 
 
